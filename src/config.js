fe50e7156b4952c8f4e270f72b772aeb2a5c5730
v-excelsior <vakyla98@gmail.com>
2020-10-21 23:30:59 +0300

    add comtacts

--- src/config.js
+++ src/config.js
@@ -3,6 +3,11 @@ const CONFIG = {
   email      : 'test@test.com',
   appName    : 'My Awesome Shop',
   firebaseURL: 'https://v-e-commerce-template.firebaseio.com',
+  otherContacts:[
+    { name:'Contact name1', data:'Contact Info1' },
+    { name:'Contact name1', data:'Contact Info2' },
+    { name:'Contact name2', data:'Contact Info3' },
+  ],
 }
 
 export default CONFIG
--- src/views/Contacts/index.js
+++ src/views/Contacts/index.js
@@ -1,18 +1,48 @@
 import React from 'react'
+import './style.scss'
 
 import { Link } from '../../components/UI'
 
+import CONFIG from '../../config'
+
 const Contacts = () => {
   return (
-    <div className="contacts">
-      <Link type='block' to='/home' className={ ['hoo', 'gaa'] }>Route</Link>
-      <Link to='/home' />
-      <Link to='/'>Route</Link>
-      <Link to='/' />
-      <Link email>My email</Link>
-      <Link email/>
-      <Link href='www.google.com'>Some text</Link>
-      <Link href='www.google.com'/>
+    <div className="container contacts">
+      <table className="contacts-table">
+        <tr>
+          <td>Email</td>
+          <td><Link email /></td>
+        </tr>
+
+        <tr>
+          <td>Number</td>
+          <td>8800-300-200</td>
+        {/*  need add copy to buffer*/}
+        </tr>
+
+        <tr>
+          <td>Address</td>
+          <td>My address</td>
+        {/*  link what open address on the map*/}
+        </tr>
+
+        { CONFIG.otherContacts?.length && CONFIG.otherContacts.map(
+          (contact, i) => (
+            <tr key={ i }>
+              <td>{ contact.name }</td>
+              <td>{ contact.data }</td>
+            </tr>
+          )
+        ) }
+        <Link type='block' to='/home' className={ ['hoo', 'gaa'] }>Route</Link>
+        <Link to='/home' />
+        <Link to='/'>Route</Link>
+        <Link to='/' />
+        <Link email>My email</Link>
+
+        <Link href='www.google.com'>Some text</Link>
+        <Link href='www.google.com' />
+      </table>
     </div>
   )
 }
