66eb1baaa25ebcb41e5b2b0f44d65162963f8d63
v-excelsior <vakyla98@gmail.com>
2020-10-22 23:24:08 +0300

    add copy to clipboard

--- src/views/Contacts/index.js
+++ src/views/Contacts/index.js
@@ -11,40 +11,61 @@ const Contacts = () => {
   const ct = 'contacts-table-cell contact-type '
   const cd = 'contacts-table-cell contact-data '
 
+  const someMethod = () => {
+    //need refactoring and improvement
+    const input = document.createElement('input')
+    input.id = 'copyBlock'
+    document.body.appendChild(input)
+
+    const copyBlock = document.querySelector('#copyBlock')
+    copyBlock.value = 'some text' + Math.random()
+
+    copyBlock.select()
+    copyBlock.setSelectionRange(0, 99999)
+
+    document.execCommand('copy')
+
+    copyBlock.remove()
+  }
+
   return (
     <div className="contacts">
       <table className="contacts-table">
         <tbody>
-          <tr className="contact">
-            <td className={ ct }>Email</td>
-            <td className={ cd }><Link email /></td>
-          </tr>
+        <tr className="contact">
+          <td className={ ct }>Email</td>
+          <td className={ cd }><Link email /></td>
+        </tr>
 
-          <tr className="contact">
-            <td className={ ct }>Telephone</td>
-            <td className={ cd + 'contact-data__telephone' }>{ telephone }</td>
-            {/*  need add copy to buffer*/ }
-          </tr>
+        <tr
+          onClick={ () => someMethod() }
+          className="contact"
+        >
+          <td className={ ct }>Telephone</td>
+          <td className={ cd + 'contact-data__telephone link' }>
+            { telephone }
+          </td>
+        </tr>
 
-          <tr className="contact">
-            <td className={ ct }>Address</td>
-            <td className={ cd }>
-              <Link href={ 'https://maps.google.com/' + address }>
-                { address }
-              </Link>
-            </td>
-            {/*  link what open address on the map*/ }
-          </tr>
+        <tr className="contact">
+          <td className={ ct }>Address</td>
+          <td className={ cd }>
+            {/*  why link not opened with address?*/ }
+            <Link href={ 'https://google.com/maps/search/' + address.replace(/\s/g, '+') }>
+              { address }
+            </Link>
+          </td>
+        </tr>
 
-          { otherContacts?.length && otherContacts.map(
-            (contact, i) => (
-              <tr className="contact" key={ i }>
-                <td className={ ct }>{ contact.type }</td>
-                <td className={ cd }>{ contact.data }</td>
-              </tr>
-            )
-          ) }
-        </tbody>
+        { otherContacts?.length && otherContacts.map(
+          (contact, i) => (
+            <tr className="contact" key={ i }>
+              <td className={ ct }>{ contact.type }</td>
+              <td className={ cd }>{ contact.data }</td>
+            </tr>
+          )
+        ) }
+      </tbody>
       </table>
     </div>
   )
