55cc3d570a4c38c9d7c9fd0a2c372ba51874d3e7
v-excelsior <vakyla98@gmail.com>
2020-10-24 23:48:01 +0300

    refactoring string cleaning helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,10 +1,8 @@
-
-
 import bubbleMessage from './bubbleMessage'
 import copyToClipboard from './copyToClipboard'
 
 const sortString = (a, b) => a.name > b.name ? 1 : -1
-const onlyNumbers = s => s.replace( /^\D+/g, '')
+const onlyNumbers = s => s.replace( /\D+/g, '')
 
 export {
   sortString,
