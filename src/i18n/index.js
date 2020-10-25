356c0c89b6e8624f3ef4fc29249e6bab39df1daa
v-excelsior <vakyla98@gmail.com>
2020-10-25 23:09:06 +0200

    refactoring dictionary mapping

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,15 +3,9 @@ import CONFIG from 'config'
 import eng from './dictionaries/eng.json'
 import rus from './dictionaries/rus.json'
 
+const dictionaries = {
+  'eng': eng,
+  'rus': rus,
+}
 
-const dictionary = (() => {
-  if (CONFIG.language === 'eng') {
-    return eng
-  }
-
-  if (CONFIG.language === 'rus') {
-    return rus
-  }
-})()
-
-export default dictionary
+export default dictionaries[CONFIG.language]
