d16bcb5c93b9585bc86482eb7ffe210714c72c24
v-excelsior <vakyla98@gmail.com>
2020-10-25 19:26:44 +0200

    refactoring header, remove link on himself

--- src/components/UI/Header/index.js
+++ src/components/UI/Header/index.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import './style.scss'
 
+import { useLocation } from 'react-router-dom'
+
 import Link from '../Link'
 
 import CONFIG from 'config'
 
 const Header = () => {
+  const isMain = useLocation().pathname === '/'
 
   return (
     <header className="header">
       <div className="header-company-name">
-        <Link to='/' type='block'>{ CONFIG.appName }</Link>
+        {
+          isMain
+            ? CONFIG.appName
+            : (
+              <Link to='/' type='block'>
+                { CONFIG.appName }
+              </Link>
+            )
+        }
       </div>
     </header>
   )
