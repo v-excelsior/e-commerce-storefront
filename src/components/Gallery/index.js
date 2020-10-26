3a053b106f5df92ee2ae5f1a11f8c27b3c030370
v-excelsior <vakyla98@gmail.com>
2020-10-26 23:25:05 +0200

    basic filtering

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -8,8 +8,8 @@ import { db } from 'services'
 const Gallery = () => {
   const [cards, setCards] = useState([])
   const [categories, setCategories] = useState([])
-  // const [filteredCards, setFilteredCards] = useState([])
-  // const [filter, setFilter] = useState('')
+  const [filteredCards, setFilteredCards] = useState([])
+  const [filter, setFilter] = useState('all')
 
   useEffect(() => {
     const fetchCards = async () => await db.getCards()
@@ -18,19 +18,35 @@ const Gallery = () => {
       setCards(res.cards)
       setCategories(res.categories)
     })
-
-    console.log('categories', categories)
   }, [])
 
-  // useEffect(() => {
-  //
-  // }, [filter])
+  useEffect(() => {
+    if (filter === 'all') {
+      setFilteredCards(cards)
+    } else {
+      setFilteredCards(cards.filter(card => {
+        console.log()
+        return card.category === filter
+      }))
+    }
+  }, [cards, filter])
 
   return (
     <div className="cards-list">
-
+      <select onChange={ (e) => {
+        console.log(e.target.value)
+        setFilter(e.target.value)
+      } }>
+        {
+          categories.map((category, i) => (
+            <option value={ category } key={ i }>
+              { category.toUpperCase() }
+            </option>
+          ))
+        }
+      </select>
       {
-        cards.map((card, i) => (<Card { ...card } key={ i } />))
+        filteredCards.map((card, i) => (<Card { ...card } key={ i } />))
       }
     </div>
   )
