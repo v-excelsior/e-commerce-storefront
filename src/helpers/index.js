d5d69b5a303af0c868780c2ebf68f24a941d2961
v-excelsior <vakyla98@gmail.com>
2020-10-29 23:58:00 +0200

    refactor sort object helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,11 +2,7 @@ import bubbleMessage from './bubbleMessage'
 import copyToClipboard from './copyToClipboard'
 
 const sortString = (a, b) => a > b ? 1 : -1
-const sortObjByProp = (prop) => {
-  return function (a, b) {
-    return a[prop] > b[prop] ? 1 : -1
-  }
-}
+const sortObjByProp = (prop) => (a, b) => a[prop] > b[prop] ? 1 : -1
 const onlyNumbers = s => s.replace(/\D+/g, '')
 
 export {
