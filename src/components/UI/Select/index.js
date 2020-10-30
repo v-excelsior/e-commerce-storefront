190af1d2820becc867e0e1f399497f6cbf07cbcf
v-excelsior <vakyla98@gmail.com>
2020-10-30 22:36:45 +0200

    add word - all

--- src/components/UI/Select/index.js
+++ src/components/UI/Select/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './style.scss'
 
+import d from 'i18n'
+
 const Select = (props) => {
   const { onChange, options, withAll } = props
 
   return (
     <select onChange={ onChange } className='vx-select'>
-      { withAll && <option value='all'>ALL</option> }
+      { withAll && <option value='all'>{ d.all.toUpperCase() }</option> }
       { options.map((option, i) => (
         <option value={ option.value } key={ i }>
           { option.label.toUpperCase() }
