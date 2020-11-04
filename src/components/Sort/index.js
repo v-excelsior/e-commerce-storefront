5975bc9ee5665fdc61d2f08955aad0bc687a36b5
v-excelsior <vakyla98@gmail.com>
2020-11-04 23:49:41 +0200

    active sort button

--- src/components/Sort/index.js
+++ src/components/Sort/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
+
 import './style.scss'
 
 import { ReactComponent as ArrowIcon } from 'assets/icons/arrow-right.svg'
 
 const Sort = (props) => {
-
   const { onAsc, onDesc, label } = props
+
+  const setSort = (target, flag) => {
+    target.classList.add('active')
+    flag ? onAsc() : onDesc() //make it as callback
+  }
 
   return (
     <div className='sort'>
@@ -16,17 +21,17 @@ const Sort = (props) => {
       ) }
 
       <button
-        onClick={ onAsc }
+        onClick={ (e) => setSort(e.target, true) }
         className='button button-sort button-ascending'
       >
-        <ArrowIcon className='button-sort-icon'/>
+        <ArrowIcon className='button-sort-icon' />
       </button>
 
       <button
-        onClick={ onDesc }
+        onClick={ (e) => setSort(e.target, false) }
         className='button button-sort button-descending'
       >
-        <ArrowIcon className='button-sort-icon'/>
+        <ArrowIcon className='button-sort-icon' />
       </button>
     </div>
   )
